2291a93985c930e3d964c0835f0fa7eb98e93aef
Oleksandr Portianko <portianko123@gmail.com>
2024-11-28 15:08:07 +0200

    base typing speed logic ready

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,26 @@ function App() {
   const [selectedText, setSelectedText] = useState<string[]>(getRandomText(150));
   const [finishedWords, setFinishedWords] = useState<TFinishedWord[]>([]);
   const [finishedWordsWidth, setFinishedWordsWidth] = useState<number>(0);
+  const [isTypingStarted, setIsTypingStarted] = useState<boolean>(false);
   const [currentWordIndex, setCurrentWordIndex] = useState<number>(0);
   const [inputTextWidth, setInputTextWidth] = useState<number>(0);
   const [wrongWord, setWrongWord] = useState<boolean>(false);
   const [inputText, setInputText] = useState<string>('');
+  const [timeEnd, setTimeEnd] = useState<boolean>(false);
+  const [timeLeft, setTimeLeft] = useState<number>(60);
+  const [accuracy, setAccuracy] = useState<number>(0);
+  const [wpm, setWpm] = useState<number>(0);
+  const [cpm, setCpm] = useState<number>(0);
 
   const finishedWordsRef = useRef<HTMLDivElement>(null);
   const currentWordsRef = useRef<HTMLDivElement>(null);
+  const textInputRef = useRef<HTMLInputElement>(null);
 
   const handleChangeValue = (value: string) => {
+    if (!isTypingStarted) {
+      setIsTypingStarted(true);
+    }
+
     setInputText(value);
 
     const currentWord = selectedText[currentWordIndex];
@@ -56,28 +67,95 @@ function App() {
     }
   }, [finishedWords, inputText])
 
+  useEffect(() => {
+    if (isTypingStarted) {
+      const correctWords = finishedWords.filter((word) => word.correct).length;
+      const totalWords = finishedWords.length;
+      const accuracy = Math.round((100 / totalWords) * correctWords);
+      const cpmData = finishedWords.map((word) => word.word.split('')).flat().length;
+
+      setAccuracy(accuracy);
+      setWpm(correctWords);
+      setCpm(cpmData);
+    }
+  }, [isTypingStarted, finishedWords]);
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+
+    if (isTypingStarted && timeLeft !== 0) {
+      interval = setInterval(() => {
+        setTimeLeft((prev) => prev - 1);
+      }, 1000);
+    } else if (isTypingStarted && timeLeft === 0) {
+      setIsTypingStarted(false);
+      setTimeEnd(true);
+    }
+
+    return () => clearInterval(interval);
+  }, [isTypingStarted, setIsTypingStarted, timeLeft]);
+
   return (
     <div className="w-full min-h-[100vh] bg-[#000000] flex flex-col justify-center items-center gap-10 p-10">
-      <h2 className="text-[60px] leading-[76px] text-[#FFFFFF] font-semibold tracking-[0.02em] max-w-[650px] text-center">
+      <h2 className="text-[60px] leading-[76px] text-[#FFFFFF] font-semibold tracking-[0.02em] max-w-[650px] text-center select-none">
         Reveal your <span className="text-[#FA00FB]">TYPING SPEED.</span> Start typing the text below.
       </h2>
-      <div className="bg-[#212121] max-w-[800px] w-full h-[80px] rounded-xl relative overflow-hidden">
+      <div className="max-w-[800px] w-full grid grid-cols-4 gap-4 select-none">
+        <div className="bg-[#FA00FB] p-4 rounded-xl">
+          <h5 className="font-semibold text-bold text-[18px] leading-[24px]">
+            Time Left
+          </h5>
+          <span className="font-bold text-[40px] leading-[56px] flex">
+            <span className="block min-w-[43px]">{timeLeft}</span>s
+          </span>
+        </div>
+        <div className="bg-[#FA00FB] p-4 rounded-xl">
+          <h5 className="font-semibold text-bold text-[18px] leading-[24px]">
+            WPM
+          </h5>
+          <span className="font-bold text-[40px] leading-[56px]">
+            {wpm}
+          </span>
+        </div>
+        <div className="bg-[#FA00FB] p-4 rounded-xl">
+          <h5 className="font-semibold text-bold text-[18px] leading-[24px]">
+            CPM
+          </h5>
+          <span className="font-bold text-[40px] leading-[56px]">
+            {cpm}
+          </span>
+        </div>
+        <div className="bg-[#FA00FB] p-4 rounded-xl">
+          <h5 className="font-semibold text-bold text-[18px] leading-[24px]">
+            % Accuracy
+          </h5>
+          <span className="font-bold text-[40px] leading-[56px]">
+            {accuracy}%
+          </span>
+        </div>
+      </div>
+      <div
+        onClick={() => textInputRef?.current?.focus()}
+        className="bg-[#212121] max-w-[800px] w-full h-[80px] rounded-xl relative overflow-hidden"
+      >
         <input
           className={`bg-transparent w-1/2 h-full text-[#FA00FB] text-[28px] leading-[36px] font-semibold tracking-[0.02em] outline-none text-end relative z-[2] select-none ${wrongWord ? 'line-through' : ''}`}
           onChange={(e) => handleChangeValue(e.target.value)}
           onKeyDown={(e) => handleKeyDown(e)}
+          ref={textInputRef}
           value={inputText}
+          readOnly={timeEnd}
           type="text"
         />
         <div
           style={{ marginLeft: `-${wrongWord ? finishedWordsWidth + 8 : finishedWordsWidth + inputTextWidth}px` }}
-          className="text-[#484747] text-[28px] leading-[36px] font-semibold tracking-[0.02em] absolute whitespace-nowrap left-1/2 top-1/2 translate-y-[-50%] h-full flex items-center mt-[1px]"
+          className="text-[#484747] text-[28px] leading-[36px] font-semibold tracking-[0.02em] absolute whitespace-nowrap left-1/2 top-1/2 translate-y-[-50%] h-full flex items-center mt-[1px] select-none"
         >
           {selectedText.join(' ')}
         </div>
         <div
           style={{ paddingRight: `${inputTextWidth}px` }}
-          className="absolute text-[28px] leading-[36px] font-semibold tracking-[0.02em] top-1/2 translate-y-[-50%] whitespace-nowrap right-1/2 mt-[1px] bg-[#212121]"
+          className="absolute text-[28px] leading-[36px] font-semibold tracking-[0.02em] top-1/2 translate-y-[-50%] whitespace-nowrap right-1/2 mt-[1px] bg-[#212121] select-none"
         >
           {finishedWords.map((word, index) => (
             <span key={index} className={word.correct ? 'text-[#484747]' : 'text-[#484747] line-through'}>
